e11d5cfe4950a14443a1e4b7e7a82ad1fbc9062d
Lee Sutton <leensutton@outlook.com>
2016-08-03 16:55:57 -0700

    Wiring up sub views.

---
+++ src/main/java/suttonsLog/Application.java
@@ -0,0 +1,16 @@
+package suttonsLog;
+
+/**
+ * Created by leens on 7/20/2016.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
