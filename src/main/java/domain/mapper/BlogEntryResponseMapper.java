b036f2a1bb50694cfd676e5acf11d9bf323ba6e5
Lee Sutton <leensutton@outlook.com>
2016-08-10 20:21:21 -0700

    Wiring up response mapper.

---
+++ src/main/java/domain/mapper/BlogEntryResponseMapper.java
@@ -0,0 +1,30 @@
+package domain.mapper;
+
+import domain.model.dbo.BlogEntryDbo;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+
+/**
+ * Created by leens on 8/10/2016.
+ */
+@Component
+public class BlogEntryResponseMapper {
+    public BlogEntryDbo mapBlogEntryResponse(ResultSet queryResult){
+        BlogEntryDbo blogEntryDbo = new BlogEntryDbo();
+
+        try {
+            blogEntryDbo.setId(queryResult.getLong("id"));
+            blogEntryDbo.setTitle(queryResult.getString("title"));
+            blogEntryDbo.setDatecreated(queryResult.getTimestamp("dateCreated"));
+            blogEntryDbo.setCategory(queryResult.getString("category"));
+            blogEntryDbo.setAuthor(queryResult.getString("author"));
+            blogEntryDbo.setContents(queryResult.getString("contents"));
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return blogEntryDbo;
+    }
+}
